49003a4c55d9115e1c99fa86e0c1d75895cca198
Jose Luis Hervas <jlcruceras@gmail.com>
2020-11-25 15:11:19 +0000

    update

--- app/controllers/ProducttypeController.php
+++ app/controllers/ProducttypeController.php
@@ -7,10 +7,21 @@ use \App\Models\ProductType;
 
 use App\Controllers\ProductController;
 
-class ProducttypeController  
-{
-    public function index()
-    {
-        $producttypes = ProductType::all();
+class ProducttypeController {
+
+    public function __construct(){
+
+        //lo creamos vacio sin datos el constructor.
+
+    }
+
+    public function index() {
+
+        //buscar la lista de productos
+        $products = ProductType::all(); //arriba pongo use ...
+        // echo "<pre>";
+        // print_r($products);
+        //generar la vista
+        include('../views/user/index.php');
     }
 }
